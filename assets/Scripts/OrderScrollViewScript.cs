2f55d64faeae5535e2b8c68e89081fa067dd3a1b
Samuel Hummel <sahummel@stud.hs-heilbronn.de>
2019-01-11 11:45:01 +0100

    menu partly working

---
+++ assets/Scripts/OrderScrollViewScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderScrollViewScript : MonoBehaviour {
+    private Text itemName;
+    private Toggle toggle;
+    private bool selected = false;
+
+    public void addItem(Dropdown dropDown)
+    {
+        itemName = dropDown.captionText;
+        if (!(itemName.text == "Getränke"))
+        {
+            //Instantiate(toggle);
+            //toggle.GetComponentInChildren<Text>().text = itemName.text;
+            //toggle.transform.SetParent(gameObject.transform, false);
+            GameObject.FindGameObjectWithTag("MenuPanel").SetActive(false);
+            this.transform.parent.gameObject.SetActive(true);
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ assets/Scripts/PanelControlScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelControlScript : MonoBehaviour {
+
+    public GameObject panel;
+
+    public void openPanel()
+    {
+        if(panel != null)
+        {
+            this.transform.parent.gameObject.SetActive(false);
+            panel.SetActive(true);
+        }
+    }
+
+    public void closePanel()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(false);
+        }
+    }
+}

95efb2cf093fc603ab278a83113f559a6fbad7ba
Samuel Hummel <sahummel@stud.hs-heilbronn.de>
2019-01-14 15:08:39 +0100

    Menü geänedert, Tooltip hinzugefügt

--- assets/Scripts/OrderScrollViewScript.cs
+++ assets/Scripts/OrderScrollViewScript.cs
@@ -3,23 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class OrderScrollViewScript : MonoBehaviour {
     private Text itemName;
     private Toggle toggle;
     private bool selected = false;
+    private string selectedOption;
+    private Text toolTip;
 
     public void addItem(Dropdown dropDown)
     {
         itemName = dropDown.captionText;
+        Debug.Log("test");
         if (!(itemName.text == "Getränke"))
         {
+            selectedOption = dropDown.captionText.text;
             //Instantiate(toggle);
             //toggle.GetComponentInChildren<Text>().text = itemName.text;
             //toggle.transform.SetParent(gameObject.transform, false);
-            GameObject.FindGameObjectWithTag("MenuPanel").SetActive(false);
-            this.transform.parent.gameObject.SetActive(true);
+            //GameObject.FindGameObjectWithTag("MenuPanel").SetActive(false);
+            //this.transform.parent.gameObject.SetActive(true);
+            Debug.Log("before 1 ....");
+            selectedOption = dropDown.captionText.text;
+            Debug.Log("before 2....");
+            dropDown.value = 0;
+            Debug.Log("before 4.... mit " + selectedOption);
+            dropDown.Hide();
         }
 
+    }
+
+    public void showToolTip(Text toolTip)
+    {
+        Debug.Log("methode aufgerufen mit " + selectedOption);
+        this.toolTip = toolTip;
+        if (!(selectedOption == "Getränke"))
+        {
+            Debug.Log("before showToolTip....");
+            toolTip.text = selectedOption + " hinzugefügt";
+            toolTip.gameObject.SetActive(true);
+            Invoke("deactivateText", 2);
+        }
+        
+    }
+
+
+    public void deactivateText()
+    {
+        toolTip.gameObject.SetActive(false);
     }
 
     void Update()
